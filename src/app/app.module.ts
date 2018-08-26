d050027e303bba634433ba42a9027b6222fcfa56
Luis Felipe <felipe096@gmail.com>
2018-08-26 15:40:41 -0500

    Added Navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { PostSectionComponent } from './post-section/post-section.component';
+import { HeaderComponent } from './shared/components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostSectionComponent
+    PostSectionComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
